f793009218e6d0d32515ed3f8c205d44917b3f00
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2025-02-13 10:21:11 +0530

    foodies project

---
+++ foodies-project/app/components/main-header/main-header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import NavLink from "./nav-link";
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} alt="A plate of food" priority />
+        NextLevel Food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink href="/meals">Browse Meals</NavLink>
+          </li>
+          <li>
+            <NavLink href="/community"> Foodies</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
